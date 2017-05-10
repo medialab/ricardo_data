8223e4e12823bb874074c82f51d9511f39f4319e
Paul Girard <paul.girard@sciencespo.fr>
2017-05-10 15:54:43 +0200

    total_reporting to total_reporting1 in new sources

---
+++ update_data_scripts/20170331_cleaning/total_reporting_cleaning.sql
@@ -0,0 +1,2 @@
+UPDATE flows SET world_trade_type='total_reporting1' WHERE world_trade_type='total_reporting';
+UPDATE flows SET world_trade_type='total_reporting2' WHERE world_trade_type='total_subreporting';
