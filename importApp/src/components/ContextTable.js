9922005f7b135f70a38d2f07b16f0dfa73e3272e
mydu <dumengying1989@gmail.com>
2019-06-03 10:56:53 +0200

    fix highlight errorColumn

---
+++ importApp/src/components/ContextTable.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const ContextTable = ({
+  className,
+  flows,
+  modificationItem
+}) => {
+  const columnNames = flows[0];
+  const {errors, field} = modificationItem;
+  return (
+    <div style={{position: 'relative', width: '100%', height: '70vh'}}>
+      
+      <div className={`action-table ${className}`}>
+        <div className={'action-table-header'}>
+          {
+            columnNames.map((columnName, index) => {
+              const errorColumn = field.indexOf(columnName) !== -1 ? true : false
+              return (
+                <div key={index} className="table-cell">
+                  <span className={errorColumn ? 'has-text-danger': 'has-text-black'}>{columnName}</span>
+                </div>
+              );
+            })
+          }
+        </div>
+        <div className={'action-table-main'}>
+            {
+              errors.map((error, rowIndex) => {
+                return (
+                  <div key={rowIndex} className="table-row">
+                    {
+                      columnNames.map((columnName, columnIndex) => {
+                        const errorColumn = field.indexOf(columnName) !== -1 ? true : false
+
+                      return (
+                        <div key={columnIndex} className="table-cell" style={{ wordBreak: 'break-all' }}>
+                          <span className={errorColumn ? 'has-text-danger': 'has-text-black'}>
+                            {flows[error.rowNumber - 1][columnIndex]}
+                          </span>
+                        </div>
+                      );
+                    })
+                  }
+                  </div>
+                );
+              })
+            }
+        </div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default ContextTable;
