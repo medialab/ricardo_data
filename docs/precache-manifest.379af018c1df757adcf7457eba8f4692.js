009b75d63950359f0442be8475a4a66841092df5
mydu <dumengying1989@gmail.com>
2019-06-07 17:34:20 +0200

    update build

---
+++ docs/precache-manifest.379af018c1df757adcf7457eba8f4692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566c38c572020a008243bf50607d8899",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2931cd46ca03bdd531d",
+    "url": "./static/css/2.4e75a13a.chunk.css"
+  },
+  {
+    "revision": "1ef8ce6126a10700bfa9",
+    "url": "./static/css/main.a0ef5007.chunk.css"
+  },
+  {
+    "revision": "a2931cd46ca03bdd531d",
+    "url": "./static/js/2.90b7aa8b.chunk.js"
+  },
+  {
+    "revision": "1ef8ce6126a10700bfa9",
+    "url": "./static/js/main.48c99efa.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
