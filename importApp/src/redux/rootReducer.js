42ec649725c141e278bf6fa364ec7c1ea7dc9c62
mydu <dumengying1989@gmail.com>
2019-06-27 12:19:45 +0200

    lint

--- importApp/src/redux/rootReducer.js
+++ importApp/src/redux/rootReducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux'
 import ui from './modules/ui';
-import auth from './modules/auth';
 import flows from './modules/flows';
 import referenceTables from './modules/referenceTables';
 import repoData from './modules/repoData';
@@ -8,7 +7,6 @@ import schemaValidation from './modules/schemaValidation';
 import modification from './modules/modification';
 export default combineReducers({
   ui,
-  auth,
   flows,
   referenceTables,
   schemaValidation,
