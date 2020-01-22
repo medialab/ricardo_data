5c5b5c05fde198aeb78f6fb117d3292a80488c4e
Paul Girard <paul.girard@sciencespo.fr>
2020-01-22 10:51:06 +0100

    Updates

---
+++ precache-manifest.9dd2d5182628696ff7ac24f887831082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bf1c10c90ac5fe63062f281893a694",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b84ce5d713e31ed1f89",
+    "url": "./static/css/2.4e75a13a.chunk.css"
+  },
+  {
+    "revision": "b84b091d6e29c1d800db",
+    "url": "./static/css/main.54446ca6.chunk.css"
+  },
+  {
+    "revision": "9b84ce5d713e31ed1f89",
+    "url": "./static/js/2.37f3d94c.chunk.js"
+  },
+  {
+    "revision": "b84b091d6e29c1d800db",
+    "url": "./static/js/main.df451b7c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
