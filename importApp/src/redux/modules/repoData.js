a74896da8085b906e85bac0cd2a33ed56a890b2a
Paul Girard <paul@ouestware.com>
2021-07-23 12:21:03 +0200

    Fixed issues in generated CSV format - comma were not escaped in flows tables - added a systematic final empty line - linted impacted files...

--- importApp/src/redux/modules/repoData.js
+++ importApp/src/redux/modules/repoData.js
@@ -1,50 +1,53 @@
-import {get, post} from 'axios';
-import {apiUri, branchUri, referenceUri, owner, repoName, repoRawContent} from '../../config/default';
-import {DEFAULT_REF_BRANCH} from '../../constants';
-
-import Octokat from 'octokat';
-
-import { Base64 } from 'js-base64';
+import { get, post } from "axios";
 import {
-  csvFormat,
-  csvParse
-} from 'd3-dsv';
+  apiUri,
+  branchUri,
+  referenceUri,
+  owner,
+  repoName,
+  repoRawContent,
+} from "../../config/default";
+import { DEFAULT_REF_BRANCH } from "../../constants";
 
-import {INIT_TABLES} from './referenceTables';
+import Octokat from "octokat";
 
-export const FETCH_TABLE_REQUEST = 'FETCH_TABLE_REQUEST';
-export const FETCH_TABLE_SUCCESS = 'FETCH_TABLE_SUCCESS';
-export const FETCH_TABLE_FAILURE = 'FETCH_TABLE_FAILURE';
+import { Base64 } from "js-base64";
+import { csvFormat, csvParse } from "d3-dsv";
 
-export const FETCH_DATAPACKAGE_REQUEST = 'FETCH_DATAPACKAGE_REQUEST';
-export const FETCH_DATAPACKAGE_SUCCESS = 'FETCH_DATAPACKAGE_SUCCESS';
-export const FETCH_DATAPACKAGE_FAILURE = 'FETCH_DATAPACKAGE_FAILURE';
+import { INIT_TABLES } from "./referenceTables";
 
-export const FETCH_TABLES_REQUEST = 'FETCH_TABLES_REQUEST';
-export const FETCH_TABLES_SUCCESS = 'FETCH_TABLES_SUCCESS';
-export const FETCH_TABLES_FAILURE = 'FETCH_TABLES_FAILURE';
+export const FETCH_TABLE_REQUEST = "FETCH_TABLE_REQUEST";
+export const FETCH_TABLE_SUCCESS = "FETCH_TABLE_SUCCESS";
+export const FETCH_TABLE_FAILURE = "FETCH_TABLE_FAILURE";
 
-export const FETCH_BRANCHES_REQUEST = 'FETCH_BRANCHES_REQUEST';
-export const FETCH_BRANCHES_SUCCESS = 'FETCH_BRANCHES_SUCCESS';
-export const FETCH_BRANCHES_FAILURE = 'FETCH_BRANCHES_FAILURE';
+export const FETCH_DATAPACKAGE_REQUEST = "FETCH_DATAPACKAGE_REQUEST";
+export const FETCH_DATAPACKAGE_SUCCESS = "FETCH_DATAPACKAGE_SUCCESS";
+export const FETCH_DATAPACKAGE_FAILURE = "FETCH_DATAPACKAGE_FAILURE";
 
-export const CREATE_BRANCH_REQUEST = 'CREATE_BRANCH_REQUEST';
-export const CREATE_BRANCH_SUCCESS = 'CREATE_BRANCH_SUCCESS';
-export const CREATE_BRANCH_FAILURE = 'CREATE_BRANCH_FAILURE';
+export const FETCH_TABLES_REQUEST = "FETCH_TABLES_REQUEST";
+export const FETCH_TABLES_SUCCESS = "FETCH_TABLES_SUCCESS";
+export const FETCH_TABLES_FAILURE = "FETCH_TABLES_FAILURE";
+
+export const FETCH_BRANCHES_REQUEST = "FETCH_BRANCHES_REQUEST";
+export const FETCH_BRANCHES_SUCCESS = "FETCH_BRANCHES_SUCCESS";
+export const FETCH_BRANCHES_FAILURE = "FETCH_BRANCHES_FAILURE";
+
+export const CREATE_BRANCH_REQUEST = "CREATE_BRANCH_REQUEST";
+export const CREATE_BRANCH_SUCCESS = "CREATE_BRANCH_SUCCESS";
+export const CREATE_BRANCH_FAILURE = "CREATE_BRANCH_FAILURE";
 
 export const LOGOUT_USER = "LOGOUT_USER";
 
-export const LOGIN_CREATE_BRANCH_REQUEST = 'LOGIN_CREATE_BRANCH_REQUEST';
-export const LOGIN_CREATE_BRANCH_SUCCESS = 'LOGIN_CREATE_BRANCH_SUCCESS';
-export const LOGIN_CREATE_BRANCH_FAILURE = 'LOGIN_CREATE_BRANCH_FAILURE';
+export const LOGIN_CREATE_BRANCH_REQUEST = "LOGIN_CREATE_BRANCH_REQUEST";
+export const LOGIN_CREATE_BRANCH_SUCCESS = "LOGIN_CREATE_BRANCH_SUCCESS";
+export const LOGIN_CREATE_BRANCH_FAILURE = "LOGIN_CREATE_BRANCH_FAILURE";
 
-export const UPDATE_REMOTE_FILES_REQUEST = 'UPDATE_REMOTE_FILES_REQUEST';
-export const UPDATE_REMOTE_FILES_LOG = 'UPDATE_REMOTE_FILES_LOG';
-export const UPDATE_REMOTE_FILES_SUCCESS = 'UPDATE_REMOTE_FILES_SUCCESS';
-export const UPDATE_REMOTE_FILES_FAILURE = 'UPDATE_REMOTE_FILES_FAILURE';
+export const UPDATE_REMOTE_FILES_REQUEST = "UPDATE_REMOTE_FILES_REQUEST";
+export const UPDATE_REMOTE_FILES_LOG = "UPDATE_REMOTE_FILES_LOG";
+export const UPDATE_REMOTE_FILES_SUCCESS = "UPDATE_REMOTE_FILES_SUCCESS";
+export const UPDATE_REMOTE_FILES_FAILURE = "UPDATE_REMOTE_FILES_FAILURE";
 
-
-const DEFAULT_MESSAGE = 'update data'
+const DEFAULT_MESSAGE = "update data";
 /**
  * ACTIONS
  */
@@ -52,251 +55,282 @@ const DEFAULT_MESSAGE = 'update data'
 export const fetchBranches = (payload) => (dispatch) => {
   dispatch({
     type: FETCH_BRANCHES_REQUEST,
-    payload
+    payload,
   });
   return get(branchUri)
-  .then((res) => {
-    return dispatch({
-      type: FETCH_BRANCHES_SUCCESS,
-      payload: {
-        ...payload,
-        branches: res.data
-      }
+    .then((res) => {
+      return dispatch({
+        type: FETCH_BRANCHES_SUCCESS,
+        payload: {
+          ...payload,
+          branches: res.data,
+        },
+      });
     })
-  }).catch((error) => dispatch({
-    type: FETCH_BRANCHES_FAILURE,
-    payload,
-    error
-  }))
-}
+    .catch((error) =>
+      dispatch({
+        type: FETCH_BRANCHES_FAILURE,
+        payload,
+        error,
+      })
+    );
+};
 
 export const fetchTable = (payload) => (dispatch) => {
-  const {branch, table} = payload
+  const { branch, table } = payload;
   dispatch({
     type: FETCH_TABLE_REQUEST,
-    payload
+    payload,
   });
   return get(`${apiUri}/${table.path}?ref=${branch}`)
-    .then((res) => dispatch({
-      type: FETCH_TABLE_SUCCESS,
-      payload: {
-        ...payload,
-        data: res.data
-      }
-    })).catch((error) => dispatch({
-      type: FETCH_TABLE_FAILURE,
-      payload,
-      error
-    }))
-}
+    .then((res) =>
+      dispatch({
+        type: FETCH_TABLE_SUCCESS,
+        payload: {
+          ...payload,
+          data: res.data,
+        },
+      })
+    )
+    .catch((error) =>
+      dispatch({
+        type: FETCH_TABLE_FAILURE,
+        payload,
+        error,
+      })
+    );
+};
 
 export const fetchAllTables = (payload) => (dispatch) => {
-  const {branch} = payload;
+  const { branch } = payload;
   // let's get the datapackage from the branch
   dispatch({
     type: FETCH_DATAPACKAGE_REQUEST,
   });
   try {
-    get(`${repoRawContent}/${branch}/datapackage.json`,  {responseType: 'json', responseEncoding: 'utf8'}).then(res => {
+    get(`${repoRawContent}/${branch}/datapackage.json`, {
+      responseType: "json",
+      responseEncoding: "utf8",
+    }).then((res) => {
       const descriptor = res.data;
-      const tablesList = descriptor.resources.filter(r => !r.group && r.name !== 'flows');
+      const tablesList = descriptor.resources.filter(
+        (r) => !r.group && r.name !== "flows"
+      );
       dispatch({
         type: FETCH_DATAPACKAGE_SUCCESS,
-        payload: descriptor
+        payload: descriptor,
       });
       // now we can get tables
       dispatch({
         type: FETCH_TABLES_REQUEST,
       });
-      Promise.all(tablesList.map((table) => {
-        return get(`${repoRawContent}/${branch}/${table.path}`, {responseType: 'text', responseEncoding: 'utf8'}).then(res => {
-          return {...table, data: res.data}; 
+      Promise.all(
+        tablesList.map((table) => {
+          return get(`${repoRawContent}/${branch}/${table.path}`, {
+            responseType: "text",
+            responseEncoding: "utf8",
+          }).then((res) => {
+            return { ...table, data: res.data };
+          });
+        })
+      )
+        .then((res) => {
+          let tables = {};
+          res.forEach((t) => {
+            tables[t.name] = t.data;
+          });
+          dispatch({
+            type: FETCH_TABLES_SUCCESS,
+            payload: tables,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          dispatch({
+            type: FETCH_TABLES_FAILURE,
+            payload,
+            error,
+          });
         });
-      }))
-      .then((res) => {
-        let tables = {};
-        res.forEach((t) => {
-          tables[t.name] = t.data
-        });
-        dispatch({
-          type: FETCH_TABLES_SUCCESS,
-          payload: tables
-        });
-      })
-      .catch((error) => { 
-        console.log(error); 
-        dispatch({
-          type: FETCH_TABLES_FAILURE,
-          payload,
-          error
-        });});
     });
-  }
-  catch(error){ 
+  } catch (error) {
     console.log(error);
     dispatch({
       type: FETCH_DATAPACKAGE_FAILURE,
-      error
+      error,
     });
   }
-}
+};
 
 export const updateRemoteFiles = (payload) => (dispatch) => {
   dispatch({
     type: UPDATE_REMOTE_FILES_REQUEST,
   });
-  const {files, branch, auth, descriptor} = payload;
+  const { files, branch, auth, descriptor } = payload;
 
   const github = new Octokat({
-    token: auth.token
+    token: auth.token,
   });
 
   dispatch(async () => {
     try {
       let repo = await github.repos(owner, repoName).fetch();
       let baseReference = await repo.git.refs(`heads/${branch}`).fetch();
-      let flowRessourceMultipart =  descriptor.resources.find((r => r.name === 'flows'))
-      let flowRessourceGroup =  descriptor.resources.find((r => r.group === 'flows'));
+      let flowRessourceMultipart = descriptor.resources.find(
+        (r) => r.name === "flows"
+      );
+      let flowRessourceGroup = descriptor.resources.find(
+        (r) => r.group === "flows"
+      );
       let treeItems = [];
       for (let file of files) {
-        
-        if (!file.sha && file.fileName.includes('flows')){
+        if (!file.sha && file.fileName.includes("flows")) {
           //new file flow ?
           //check if file already exists
           dispatch({
             type: UPDATE_REMOTE_FILES_LOG,
-            payload: `downloading existing flows file ${file.fileName}`
+            payload: `downloading existing flows file ${file.fileName}`,
           });
           try {
-            const exists = await get(`${repoRawContent}/${branch}/data/${file.fileName}`,{ responseType: 'text', responseEncoding: 'utf8'})
+            const exists = await get(
+              `${repoRawContent}/${branch}/data/${file.fileName}`,
+              { responseType: "text", responseEncoding: "utf8" }
+            );
             if (exists.status === 200) {
               // append new rows at end of the existing file
-              file.data = csvParse(exists.data).concat(file.data) 
+              file.data = csvParse(exists.data).concat(file.data);
             }
           } catch (error) {
             if (error.response && error.response.status === 404) {
               // that's a 404 error which is fine
               // it's a new file, add it to the datapackage see issue #70
               if (flowRessourceMultipart) {
-                flowRessourceMultipart.path.push(`data/${file.fileName}`)
-              }
-              else if(flowRessourceGroup) {
-                const newRessource =  {...flowRessourceGroup};
+                flowRessourceMultipart.path.push(`data/${file.fileName}`);
+              } else if (flowRessourceGroup) {
+                const newRessource = { ...flowRessourceGroup };
                 newRessource.path = `/data/${file.fileName}`;
                 newRessource.title = file.source;
                 descriptor.resources.push(newRessource);
               }
-            }
-            else
-              console.log(error);  
-              dispatch({
-                type: UPDATE_REMOTE_FILES_FAILURE,
-                error
-              });          
-          }  
+            } else console.log(error);
+            dispatch({
+              type: UPDATE_REMOTE_FILES_FAILURE,
+              error,
+            });
+          }
         }
         dispatch({
           type: UPDATE_REMOTE_FILES_LOG,
-          payload: `uploading ${file.fileName}`
+          payload: `uploading ${file.fileName}`,
         });
-        let fileGit = await repo.git.blobs.create({content: Base64.encode(csvFormat(file.data)), encoding: 'base64'});
+        let fileGit = await repo.git.blobs.create({
+          content: Base64.encode(csvFormat(file.data) + "\n"),
+          encoding: "base64",
+        });
         let filePath = `data/${file.fileName}`;
         treeItems.push({
           path: filePath,
           sha: fileGit.sha,
           mode: "100644",
-          type: "blob"
-        })
-      } 
+          type: "blob",
+        });
+      }
       // commit new version of the datapackage
       dispatch({
         type: UPDATE_REMOTE_FILES_LOG,
-        payload: `uploading datapackage`
+        payload: `uploading datapackage`,
       });
-      let fileGit = await repo.git.blobs.create({content: Base64.encode(JSON.stringify(descriptor, null, 2)), encoding: 'base64'});
+      let fileGit = await repo.git.blobs.create({
+        content: Base64.encode(JSON.stringify(descriptor, null, 2)),
+        encoding: "base64",
+      });
       let filePath = `datapackage.json`;
       treeItems.push({
         path: filePath,
         sha: fileGit.sha,
         mode: "100644",
-        type: "blob"
-      })
+        type: "blob",
+      });
       dispatch({
         type: UPDATE_REMOTE_FILES_LOG,
-        payload: `creating tree`
+        payload: `creating tree`,
       });
       let tree = await repo.git.trees.create({
         tree: treeItems,
-        base_tree: baseReference.object.sha
+        base_tree: baseReference.object.sha,
       });
       dispatch({
         type: UPDATE_REMOTE_FILES_LOG,
-        payload: `creating commit`
+        payload: `creating commit`,
       });
       let commit = await repo.git.commits.create({
         message: auth.message || DEFAULT_MESSAGE,
         tree: tree.sha,
-        parents: [baseReference.object.sha]
+        parents: [baseReference.object.sha],
       });
-  
-      baseReference.update({sha: commit.sha});
+
+      baseReference.update({ sha: commit.sha });
       dispatch({
         type: UPDATE_REMOTE_FILES_SUCCESS,
-        payload: commit.sha
+        payload: commit.sha,
       });
-    } catch(err) {
+    } catch (err) {
       console.error(err);
       dispatch({
         type: UPDATE_REMOTE_FILES_FAILURE,
-        err
+        err,
       });
     }
-  })
-}
+  });
+};
 
 export const createBranch = (payload) => (dispatch) => {
   dispatch({
-    type: CREATE_BRANCH_REQUEST
+    type: CREATE_BRANCH_REQUEST,
   });
 
-  const {auth, branch, reference} = payload;
+  const { auth, branch, reference } = payload;
   const data = {
-    "ref": `refs/heads/${branch}`,
-    "sha": reference.sha
+    ref: `refs/heads/${branch}`,
+    sha: reference.sha,
   };
-  
+
   return post(referenceUri, data, {
     auth: {
       username: auth.username,
-      password: auth.token
-    }
+      password: auth.token,
+    },
   })
-  .then((res) => dispatch({
-    type: CREATE_BRANCH_SUCCESS,
-    payload: {
-      name: auth.username,
-      ref: res.data
-    }
-  })).catch((error) => dispatch({
-    type: CREATE_BRANCH_FAILURE,
-    error
-  }))
-}
+    .then((res) =>
+      dispatch({
+        type: CREATE_BRANCH_SUCCESS,
+        payload: {
+          name: auth.username,
+          ref: res.data,
+        },
+      })
+    )
+    .catch((error) =>
+      dispatch({
+        type: CREATE_BRANCH_FAILURE,
+        error,
+      })
+    );
+};
 
 export const logoutUser = () => ({
-  type: LOGOUT_USER
-})
+  type: LOGOUT_USER,
+});
 
-
-export const  loginCreateBranch = (payload) => (dispatch) => {
+export const loginCreateBranch = (payload) => (dispatch) => {
   dispatch({
     type: LOGIN_CREATE_BRANCH_REQUEST,
-    payload
-  })
-  const {token} = payload;
+    payload,
+  });
+  const { token } = payload;
 
   const github = new Octokat({
-    token: token
+    token: token,
   });
 
   dispatch(async () => {
@@ -305,119 +339,119 @@ export const  loginCreateBranch = (payload) => (dispatch) => {
       const username = userInfo.login;
       let repo = await github.repos(owner, repoName).fetch();
       let branches = await repo.branches.fetch();
-      let selectedBranch = branches.items.find((branch) => branch.name === username);
+      let selectedBranch = branches.items.find(
+        (branch) => branch.name === username
+      );
       if (!selectedBranch) {
-        const refBranch = branches.items.find((branch) => branch.name === DEFAULT_REF_BRANCH);
+        const refBranch = branches.items.find(
+          (branch) => branch.name === DEFAULT_REF_BRANCH
+        );
         selectedBranch = await repo.git.refs.create({
           ref: `refs/heads/${username}`,
-          sha: refBranch.commit.sha
-        })
+          sha: refBranch.commit.sha,
+        });
       }
       dispatch({
         type: LOGIN_CREATE_BRANCH_SUCCESS,
         payload: {
           name: username,
-          ref: selectedBranch
-        }
+          ref: selectedBranch,
+        },
       });
-    } catch(error) {
-      console.error(error)
+    } catch (error) {
+      console.error(error);
       dispatch({
         type: LOGIN_CREATE_BRANCH_FAILURE,
         payload: {
-          error
-        }
-      })
+          error,
+        },
+      });
     }
-  })
-  
-
-}
+  });
+};
 
 /**
  * REDUCER
  */
 
-const initialState = {
+const initialState = {};
 
-}
-
-export default function reducer(state = initialState, action){
-  const {payload} = action;
-  switch (action.type){
+export default function reducer(state = initialState, action) {
+  const { payload } = action;
+  switch (action.type) {
     case LOGOUT_USER:
       return {
         ...state,
         isLogined: false,
         tables: null,
-        selectedBranch:null,
-        isBranchCreated: false
-      }
+        selectedBranch: null,
+        isBranchCreated: false,
+      };
     case INIT_TABLES:
       return {
         ...state,
         tables: null,
-        remoteUpdateStatus: null
-      }
+        remoteUpdateStatus: null,
+      };
     case FETCH_TABLES_SUCCESS:
       return {
         ...state,
-        tables: payload
-      }
+        tables: payload,
+      };
     case FETCH_TABLES_FAILURE:
       return {
         ...state,
-        tables: null
-      }
+        tables: null,
+      };
     case FETCH_DATAPACKAGE_SUCCESS:
       return {
         ...state,
-        descriptor: payload
-      }
+        descriptor: payload,
+      };
     case FETCH_BRANCHES_SUCCESS:
       return {
         ...state,
-        branches: payload.branches
-      }
+        branches: payload.branches,
+      };
     case LOGIN_CREATE_BRANCH_SUCCESS:
       return {
         ...state,
         isLogined: true,
         selectedBranch: payload,
         isBranchCreated: true,
-      }
+      };
     case LOGIN_CREATE_BRANCH_FAILURE:
       return {
         ...state,
         isLogined: false,
         selectedBranch: null,
-        isBranchCreated: false
-      }
+        isBranchCreated: false,
+      };
     case UPDATE_REMOTE_FILES_REQUEST:
       return {
         ...state,
-        remoteUpdateStatus: 'loading',
+        remoteUpdateStatus: "loading",
         lastCommit: null,
-        remoteUpdateMessage: null
-      }
+        remoteUpdateMessage: null,
+      };
     case UPDATE_REMOTE_FILES_LOG:
-        return {
-          ...state,
-          remoteUpdateStatus: 'loading',
-          remoteUpdateMessage: payload 
-        }
+      return {
+        ...state,
+        remoteUpdateStatus: "loading",
+        remoteUpdateMessage: payload,
+      };
     case UPDATE_REMOTE_FILES_SUCCESS:
       return {
         ...state,
         remoteUpdateStatus: "updated",
-        lastCommit: payload
-      }
+        lastCommit: payload,
+      };
     case UPDATE_REMOTE_FILES_FAILURE:
       return {
         ...state,
-        remoteUpdateStatus: "fail"
-      }
+        remoteUpdateStatus: "fail",
+      };
     default:
-      return state
+      return state;
   }
 }
--- importApp/src/utils/fileExporter.js
+++ importApp/src/utils/fileExporter.js
@@ -1,49 +1,42 @@
-import XLSX from 'xlsx';
-import {
-  csvFormatRows,
-  csvFormat
-} from 'd3-dsv';
+import XLSX from "xlsx";
+import { csvFormatRows, csvFormat } from "d3-dsv";
 
-import FileSaver from 'file-saver';
+import FileSaver from "file-saver";
 
-export function downloadFlow (array, fileName, ext) {
+export function downloadFlow(array, fileName, ext) {
   let file;
-  const header = array[0]
-  switch(ext) {
-    case 'csv':
+  const header = array[0];
+  switch (ext) {
+    case "csv":
     default: {
-      const csvString = csvFormatRows(array)
-      file = new File(
-        [csvString],
-        `${fileName}.${ext}`,
-        { type: 'text/csv;charset=utf-8' }
-      )
-      FileSaver.saveAs(file)
-      break
+      const csvString = csvFormatRows(array) + "\n";
+      file = new File([csvString], `${fileName}.${ext}`, {
+        type: "text/csv;charset=utf-8",
+      });
+      FileSaver.saveAs(file);
+      break;
     }
-    case 'xlsx': {
-      const sheet = XLSX.utils.aoa_to_sheet(array, {header});
+    case "xlsx": {
+      const sheet = XLSX.utils.aoa_to_sheet(array, { header });
       const wb = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(wb, sheet, 'SheetJS');
+      XLSX.utils.book_append_sheet(wb, sheet, "SheetJS");
       XLSX.writeFile(wb, `${fileName}.${ext}`);
-      break
+      break;
     }
   }
 }
 
-export function downloadTable (array, fileName, ext) {
+export function downloadTable(array, fileName, ext) {
   let file;
-  switch(ext) {
-    case 'csv': 
-    default:{
-      const csvString = csvFormat(array)
-      file = new File(
-        [csvString],
-        `${fileName}.${ext}`,
-        { type: 'text/csv;charset=utf-8' }
-      )
-      FileSaver.saveAs(file)
-      break
+  switch (ext) {
+    case "csv":
+    default: {
+      const csvString = csvFormat(array);
+      file = new File([csvString], `${fileName}.${ext}`, {
+        type: "text/csv;charset=utf-8",
+      });
+      FileSaver.saveAs(file);
+      break;
     }
   }
 }
