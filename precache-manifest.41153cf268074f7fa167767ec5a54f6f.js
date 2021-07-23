040d7285404415fb6578a235dd924bc1cbc9ce07
Paul Girard <paul@ouestware.com>
2021-07-23 12:24:21 +0200

    Updates

---
+++ precache-manifest.41153cf268074f7fa167767ec5a54f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845cbb3be5769094377f4c2ca5b91ede",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34497056b9843dedc59e",
+    "url": "./static/css/2.4e75a13a.chunk.css"
+  },
+  {
+    "revision": "185881b8f7fc603cc224",
+    "url": "./static/css/main.54446ca6.chunk.css"
+  },
+  {
+    "revision": "34497056b9843dedc59e",
+    "url": "./static/js/2.00a14a58.chunk.js"
+  },
+  {
+    "revision": "185881b8f7fc603cc224",
+    "url": "./static/js/main.8f9c883a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
