5535b5e4899160f9f29e44d81bbc047db62d9640
Paul Girard <paul@ouestware.com>
2021-08-02 22:15:30 +0200

    Updates

---
+++ precache-manifest.0946f25fa037cee66176a73ee2f9a3c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb0c8fdbe1b08d0013f9cae6d2e1ca4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43920c00e95d21290b43",
+    "url": "./static/css/2.4e75a13a.chunk.css"
+  },
+  {
+    "revision": "51eeff8a6927c54aaada",
+    "url": "./static/css/main.54446ca6.chunk.css"
+  },
+  {
+    "revision": "43920c00e95d21290b43",
+    "url": "./static/js/2.5fbfb8b7.chunk.js"
+  },
+  {
+    "revision": "51eeff8a6927c54aaada",
+    "url": "./static/js/main.ae854c87.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.41153cf268074f7fa167767ec5a54f6f.js"
+  "./precache-manifest.0946f25fa037cee66176a73ee2f9a3c7.js"
 );
 
 self.addEventListener('message', (event) => {
