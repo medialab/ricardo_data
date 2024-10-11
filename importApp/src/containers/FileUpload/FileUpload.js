62dfff712fa3c425aebd4224d8c3a6344359c73a
Paul Girard <paul@ouestware.com>
2024-10-11 17:18:52 +0200

    [import App] make sure to always load last versions of tables - cache buster in fetchalltables (and all get call) - make suree to fetch tables after upload - document the right version of node in package.json

--- importApp/src/containers/FileUpload/FileUpload.js
+++ importApp/src/containers/FileUpload/FileUpload.js
@@ -1,59 +1,54 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {findIndex} from 'lodash';
+import { findIndex } from "lodash";
+import React from "react";
+import { connect } from "react-redux";
 
-import {Button, DropZone} from 'design-workshop'
-import { 
-  importFlows,
-} from '../../redux/modules/flows';
+import { Button, DropZone } from "design-workshop";
+import { importFlows } from "../../redux/modules/flows";
 
-import { 
-  setStep
-} from '../../redux/modules/ui';
+import { setStep } from "../../redux/modules/ui";
 
 import {
-  validateHeader, 
-  getResourceSchema
-} from '../../redux/modules/schemaValidation';
+  getResourceSchema,
+  validateHeader,
+} from "../../redux/modules/schemaValidation";
 
-import DataPrep from '../DataPrep';
+import DataPrep from "../DataPrep";
 
-import HeaderValidation from '../../components/HeaderValidation';
+import HeaderValidation from "../../components/HeaderValidation";
 
-import {parseSheet, parseTable} from '../../utils/fileParser';
-import { MAXIMUM_FILE_SIZE } from '../../constants';
+import { MAXIMUM_FILE_SIZE } from "../../constants";
+import { fetchAllTables } from "../../redux/modules/repoData";
+import { parseSheet, parseTable } from "../../utils/fileParser";
 
 const prepareFlowData = (data, schema) => {
   // lower foreignKeys
-  const FKFieldsIndices = schema.foreignKeys.map(fk => Array.isArray(fk.fields) ? fk.fields : [fk.fields]).reduce((set, fields) => {
-    //unique map results
-    fields.forEach(f => {
-      // hardcoded exception...
-      if (f !== 'source') {
-        const i = data[0].indexOf(f)
-        if (!set.includes(i))
-          set.push(i);
-        };
-      }
-    );
-    return set;
-  }, []);
+  const FKFieldsIndices = schema.foreignKeys
+    .map((fk) => (Array.isArray(fk.fields) ? fk.fields : [fk.fields]))
+    .reduce((set, fields) => {
+      //unique map results
+      fields.forEach((f) => {
+        // hardcoded exception...
+        if (f !== "source") {
+          const i = data[0].indexOf(f);
+          if (!set.includes(i)) set.push(i);
+        }
+      });
+      return set;
+    }, []);
 
   // lowercase Reporting and partner + strip
- return data.map( (d,i) => {
+  return data.map((d, i) => {
     if (i !== 0) {
-      return d.map( (v,i) => {
+      return d.map((v, i) => {
         // Foreign Key fields are forced lowercase and trimed to limit glue work
         if (FKFieldsIndices.includes(i) && v.toLowerCase)
           return v.toLowerCase().trim();
-        else
-          return v;
-      })
-    }
-    else // don't touch headers..
-      return d
-  })
-}
+        else return v;
+      });
+    } // don't touch headers..
+    else return d;
+  });
+};
 
 const FileUpload = ({
   steps,
@@ -64,110 +59,124 @@ const FileUpload = ({
   headerFeedback,
   setStep,
   importFlows,
-  validateHeader
+  validateHeader,
+  repoData,
+  fetchAllTables,
 }) => {
   const handleDrop = ([file]) => {
-    if (file.name.split('.')[1] === 'xlsx') {
+    if (file.name.split(".")[1] === "xlsx") {
       parseSheet(file)
-      .then((data) => {
-        data = prepareFlowData(data, schema);
-        importFlows({
-          file: {
-            name: file.name.split('.')[0]
-          },
-          data
+        .then((data) => {
+          data = prepareFlowData(data, schema);
+          importFlows({
+            file: {
+              name: file.name.split(".")[0],
+            },
+            data,
+          });
+          validateHeader({ source: data, schema });
+          fetchAllTables({ branch: repoData.selectedBranch.name });
+        })
+        .catch((error) => {
+          console.error(error);
+          console.error("fail to parse file");
         });
-        validateHeader({source: data, schema})
-      })
-      .catch((error) => {
-        console.error(error)
-        console.error('fail to parse file')
-      })
-    }
-    else {
+    } else {
       parseTable(file)
-      .then((data) => {
-        data = prepareFlowData(data, schema);
-        importFlows({
-          file: {
-            name: file.name
-          },
-          data
+        .then((data) => {
+          data = prepareFlowData(data, schema);
+          importFlows({
+            file: {
+              name: file.name,
+            },
+            data,
+          });
+          validateHeader({ source: data, schema });
+
+          fetchAllTables({ branch: repoData.selectedBranch.name });
+        })
+        .catch((error) => {
+          console.error(error);
+          console.error("fail to parse file");
         });
-        validateHeader({source: data, schema})
-      })
-      .catch((error) => {
-        console.error(error)
-        console.error('fail to parse file')
-      })
     }
-  }
+  };
   const handleDropRejected = (file, event) => {
-    console.log("file is invalid")
-  }
+    console.log("file is invalid");
+  };
   const handleNextStep = () => {
     const currentIndex = findIndex(steps, selectedStep);
-    setStep(steps[currentIndex+1])
-  }
+    setStep(steps[currentIndex + 1]);
+  };
   return (
     <div>
       <DataPrep />
-      {
-        tables &&
+      {tables && (
         <DropZone
           maxSize={MAXIMUM_FILE_SIZE}
           onDrop={handleDrop}
           onDropRejected={handleDropRejected}
-          isSize="small">
-          <span className="tech-info">Cick here to upload or drag and drop .xlsx, .csv file here(maximum 10MB)</span>
+          isSize="small"
+        >
+          <span className="tech-info">
+            Cick here to upload or drag and drop .xlsx, .csv file here(maximum
+            10MB)
+          </span>
         </DropZone>
-      }
-      {
-        headerFeedback && headerFeedback.status === 'loading' &&
+      )}
+      {headerFeedback && headerFeedback.status === "loading" && (
         <span>Validating Headers</span>
-      }
-      {
-        headerFeedback && headerFeedback.valid &&
-        <div style={{
-          display: 'flex',
-          justifyContent: 'space-between'
-          }}>
-          <span className="has-text-success has-text-weight-bold">Headers of "{flows.file.name}" are valid</span>
-          <Button 
-            isColor="info" 
-            onClick={handleNextStep}>
-              Next Step
+      )}
+      {headerFeedback && headerFeedback.valid && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <span className="has-text-success has-text-weight-bold">
+            Headers of "{flows.file.name}" are valid
+          </span>
+          <Button isColor="info" onClick={handleNextStep}>
+            Next Step
           </Button>
         </div>
-      }
-      {
-        headerFeedback && !headerFeedback.valid && headerFeedback.type === 'ERROR_HEADER' &&
-        <div style={{
-          textAlign:'center'
-          }}>
-          <span className="has-text-danger has-text-weight-bold">Headers of "{flows.file.name}" do not match schema fields, please fix your file and re-upload</span>
-          <HeaderValidation 
-            headerNames = {headerFeedback.headerNames}
-            fieldNames = {headerFeedback.fieldNames}
-          />
-        </div>
-      }
+      )}
+      {headerFeedback &&
+        !headerFeedback.valid &&
+        headerFeedback.type === "ERROR_HEADER" && (
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <span className="has-text-danger has-text-weight-bold">
+              Headers of "{flows.file.name}" do not match schema fields, please
+              fix your file and re-upload
+            </span>
+            <HeaderValidation
+              headerNames={headerFeedback.headerNames}
+              fieldNames={headerFeedback.fieldNames}
+            />
+          </div>
+        )}
     </div>
-  )
-}
+  );
+};
 
-
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   steps: state.ui.steps,
   selectedStep: state.ui.selectedStep,
   schema: state.repoData.descriptor && getResourceSchema(state),
   flows: state.flows,
   tables: state.repoData.tables,
-  headerFeedback: state.schemaValidation.headerFeedback
- })
- 
- export default connect(mapStateToProps, {
+  headerFeedback: state.schemaValidation.headerFeedback,
+  repoData: state.repoData,
+});
+
+export default connect(mapStateToProps, {
   importFlows,
   setStep,
-  validateHeader
- })(FileUpload);
+  validateHeader,
+  fetchAllTables,
+})(FileUpload);
